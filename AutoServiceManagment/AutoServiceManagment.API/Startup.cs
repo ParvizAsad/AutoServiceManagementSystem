68deeec9680c8904c6de76bef38e137ca821d875
ParvizAsad <86598426+ParvizAsad@users.noreply.github.com>
2022-04-18 16:21:53 +0400

    Global Error Handling and BrandService update

--- AutoServiceManagment/AutoServiceManagment.API/Startup.cs
+++ AutoServiceManagment/AutoServiceManagment.API/Startup.cs
@@ -18,6 +18,7 @@ using AutoServiceManagment.Repository.Repository;
 using AutoServiceManagment.Services.Services.Contracts;
 using AutoServiceManagment.Services.Services;
 using Microsoft.EntityFrameworkCore;
+using AutoServiceManagment.Infrastructure.Middlewares;
 
 namespace AutoServiceManagment.API
 {
@@ -89,6 +90,7 @@ namespace AutoServiceManagment.API
             }
 
             app.UseHttpsRedirection();
+            app.ConfigureExceptionHandler();
 
             app.UseRouting();
 
