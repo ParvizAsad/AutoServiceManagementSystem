3eb17c8332bd0c2591ba217122214fa83f9c844b
ParvizAsad <86598426+ParvizAsad@users.noreply.github.com>
2022-04-19 12:51:16 +0400

    Add  Serilog

--- AutoServiceManagment/AutoServiceManagment.API/Program.cs
+++ AutoServiceManagment/AutoServiceManagment.API/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +15,38 @@ namespace AutoServiceManagment.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                  .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                  .Enrich.FromLogContext()
+                  .WriteTo.File(
+                      System.IO.Path.Combine("D:\\LogFiles", "AutoService", "diagnostics.txt"),
+                      rollingInterval: RollingInterval.Day,
+                      fileSizeLimitBytes: 10 * 1024 * 1024,
+                      retainedFileCountLimit: 30,
+                      rollOnFileSizeLimit: true,
+                      shared: true,
+                      flushToDiskInterval: TimeSpan.FromSeconds(2))
+                  .WriteTo.Console()
+                  .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- AutoServiceManagment/AutoServiceManagment.Services/Services/ServiceService.cs
+++ AutoServiceManagment/AutoServiceManagment.Services/Services/ServiceService.cs
@@ -57,7 +57,6 @@ namespace AutoServiceManagment.Services.Services
         public async Task UpdateServiceAsyncId(int? id, ServiceDto serviceDto)
         {
             var service = await DbContext.Services.FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == true);
-
             if (service == null) { throw new Exception("Service not found!"); }
 
             service = _mapper.Map<Service>(serviceDto);
